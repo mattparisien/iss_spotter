e098fb690fb1fd13537e00c5c175d247756120a3
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-20 23:00:44 +0000

    Adding a printTimes function call inside of nextISSTimesForMyLocation function call. This app now prints the times succesfully with proper sentence syntax.

--- index.js
+++ index.js
@@ -33,15 +33,6 @@ const {
 //     });
 
 
-    
-
-nextISSTimesForMyLocation((error, times) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  console.log(times);
-});
-
 const printTimes = function(times) {
   for (const pass of times) {
     const datetime = new Date(0);
@@ -50,4 +41,11 @@ const printTimes = function(times) {
     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
 };
+
+nextISSTimesForMyLocation((error, times) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printTimes(times)
+});
 
