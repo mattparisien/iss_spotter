3dc1830f5015c615c4dfc2114a9f3bb19342c11b
Matthew Parisien <matthewparisien4@gmail.com>
2021-08-21 11:50:36 +0000

    Implementing my second function, fetchCoordsByIP, this functions uses the body returned from fetchMyIP, parses it into a js object, and extracts the ip from it - to then inject it into the request to the freegeoip api.

--- iss_promised.js
+++ iss_promised.js
@@ -4,4 +4,9 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org/?format=json'); //returns promise of request for ip data, returned as a JSON string
 };
 
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${body}`)
+}
+
 module.exports = { fetchMyIP };
